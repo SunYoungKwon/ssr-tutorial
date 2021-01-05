8b08100b4c9f864440a0048bc3f2a99b878b3972
sunyoung <sunnyk5780@gmail.com>
2021-01-05 23:41:35 +0900

    Add server code to index.server.js using express

--- src/index.server.js
+++ src/index.server.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom';
+import App from './App';
 
-const html = ReactDOMServer.renderToString(<div>Hello SSR!</div>);
+const app = express();
 
-console.log(html);
+const serverRender = (req, res, next) => {
+  // 404가 떠야하는 상황에서 404를 띄우지 않고 서버 사이드 렌더링을 해 줌
+
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx);
+  res.send(root);
+};
+
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log('Running on http://localhost:5000');
+});
